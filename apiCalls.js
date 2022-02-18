2f1e878254181447d224ee22a5ea21630235c505
hari <srihari.kappera@appmajix.com>
2022-02-18 14:48:18 +0530

    changes

--- apiCalls.js
+++ apiCalls.js
@@ -4,8 +4,8 @@ const dev = process.env.NODE_ENV !== "production";
 // const server = dev
 //   ? "http://localhost:8000/backend"
 //   : "https://tkadmin.mindmajixtechhub.com/backend";
-const server = "https://tkadmin.mindmajixtechhub.com/backend";
 
+const server = "https://tkadmin.mindmajixtechhub.com/backend";
 export const getTrendingCourses = async (path) => {
   try {
     const courses = await axios.get(`${server}/home/${path}`);
